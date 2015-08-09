9154a3cc0ba8a8da3e39b3dea20c8f5a72676283
Tuarender <tuarender005@gmail.com>
2015-08-10 00:17:11 +0700

    Update Corousel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Khbicycle</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -43,14 +43,71 @@
       </div>
     </nav>
 
-    <div class="container">
+    <div class="container" style="padding:50px 0px">
 
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="images/carousel/slide1.jpg" alt="Chania" width="1024">
+          </div>
+          <div class="item">
+            <img src="images/carousel/slide2.jpg" alt="Chania" width="1024">
+          </div>
+          <div class="item">
+            <img src="images/carousel/slide3.jpg" alt="Flower" width="1024">
+          </div>
+        </div>
+
       </div>
 
-    </div><!-- /.container -->
+    </div>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+      <br>
+
+    <!-- /.container -->
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
