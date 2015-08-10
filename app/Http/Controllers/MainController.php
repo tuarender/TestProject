5246b36e54df5bf027f85d1b430b4a292a2bebba
Tuarender <tuarender005@gmail.com>
2015-08-10 20:29:48 +0700

    Remove BaseController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,7 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\View;
 
-class MainController extends BaseController
+class MainController extends Controller
 {
 
     public function main()
